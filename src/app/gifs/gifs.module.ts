5725cd5f4c927683d9b4a81b356a71f26c01041f
doli.mia.fate.s@gmail.com <doli.mia.fate.s@gmail.com>
2024-11-25 16:37:59 +0100

    Nuevo Componente: Tarjetas

--- src/app/gifs/gifs.module.ts
+++ src/app/gifs/gifs.module.ts
@@ -5,13 +5,15 @@ import { HomePageComponent } from './pages/home/home-page.component';
 import { SearchBoxComponent } from './components/search-box/search-box.component';
 import { CardListComponent } from './components/card-list/card-list.component';
 import { GifsService } from './services/gifs.service';
+import { CardComponent } from './components/card/card.component';
 
 
 @NgModule({
   declarations: [
     HomePageComponent,
     SearchBoxComponent,
-    CardListComponent
+    CardListComponent,
+    CardComponent
   ],
   imports: [
     CommonModule
